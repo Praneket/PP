265342a55d54196d035cffca68ccf28b6da15f75
Praneket <jadhavpraneket@gmail.com>
2025-12-09 23:35:48 +0530

    changed profile pic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
         transition={{ type: "spring", stiffness: 50, damping: 10 }}
         viewport={{ once: true }}
       >
-        <img src="/assets/profile-square-12.jpg" alt="About Me Illustration" />
+        <img src="/assets/profile photo circle.png" alt="About Me Illustration" />
       </motion.div>
 
       <motion.div
