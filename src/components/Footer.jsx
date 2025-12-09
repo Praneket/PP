8785630dd7c3768d3fd5b69eaf174dc87f1efbb4
Praneket <jadhavpraneket@gmail.com>
2025-12-09 23:49:24 +0530

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ export default function Footer() {
       <img className='invert h-5 lg:h-9' src="/assets/Picsart_25-11-09_19-57-31-938.png" alt="" />
 
       <div className='text-white lg:font-semibold lg:text-sm font-normal text-[10px] text-right lg:space-y-3'>
-        <p>@ 2026 Personal Portfolio</p>
-        <p>Made by -PJ</p>
+        {/* <p></p> */}
+        <p>@ 2026 Personal Portfolio Made by -PJ</p>
       </div>
     </div>
   )
